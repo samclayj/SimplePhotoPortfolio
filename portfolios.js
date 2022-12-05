60bb13d40b915e5296cdaea173228c90944deec7
Samuel Jentsch <samjentsch@Samuels-MBP.attlocal.net>
2022-12-05 11:54:56 -0600

    test

---
+++ portfolios.js
@@ -0,0 +1,14 @@
+/**
+ * List portfolios here.
+ */
+ 
+const fearOfWater = [
+
+];
+
+
+window.onload = () => {
+const body = document.querySelector('.test');
+body.backgroundColor = 'red';
+console.log('sup');
+}
