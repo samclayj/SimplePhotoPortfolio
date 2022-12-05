0a27aa2c4feb5aec06d8bcf16118e21392d8e990
Samuel Jentsch <samjentsch@Samuels-MBP.attlocal.net>
2022-12-05 17:14:45 -0600

    First additions.

--- portfolios.js
+++ portfolios.js
@@ -1,14 +1,60 @@
 /**
  * List portfolios here.
  */
- 
 const fearOfWater = [
-
+    'Meeting 5 - Sam Jentsch 01.jpg',
+    'Meeting 5 - Sam Jentsch 02.jpg',
+    'Meeting 5 - Sam Jentsch 03.jpg',
+    'Meeting 5 - Sam Jentsch 04.jpg',
+    'Meeting 5 - Sam Jentsch 05.jpg',
+    'Meeting 5 - Sam Jentsch 06.jpg',
+    'Meeting 5 - Sam Jentsch 07.jpg',
+    'Meeting 5 - Sam Jentsch 08.jpg',
+    'Meeting 5 - Sam Jentsch 09.jpg',
+    'Meeting 5 - Sam Jentsch 10.jpg',
+    'Meeting 5 - Sam Jentsch 11.jpg',
+    'Meeting 5 - Sam Jentsch 12.jpg',
+    'Meeting 5 - Sam Jentsch 13.jpg',
 ];
 
+let galleryElement;
+let currentGallery = fearOfWater;
+let galleryIndex = 0;
 
+
+document.onkeydown = checkKey;
+// document.onclick = nextImage;
+
+function setImage(imagePath) {
+    const url = "url('static/fear_of_water/" + imagePath +"')";
+    galleryElement.style.backgroundImage = url;
+}
+
+function nextImage() {
+    galleryIndex = (galleryIndex + 1) % currentGallery.length;
+    setImage(currentGallery[galleryIndex]);
+}
+
+function prevImage() {
+    galleryIndex = galleryIndex != 0 ? galleryIndex - 1 : currentGallery.length - 1;
+    setImage(currentGallery[galleryIndex]);
+}
+
+function checkKey(e) {
+    console.log('checkling');
+    if (e.keyCode == '37') {
+       // left arrow
+        prevImage();
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+        nextImage();
+    }
+}
+ 
 window.onload = () => {
-const body = document.querySelector('.test');
-body.backgroundColor = 'red';
-console.log('sup');
+    console.log('sup');
+    galleryIndex = 0;
+    galleryElement = document.getElementById('gallery');
+    setImage(currentGallery[0]);
 }
