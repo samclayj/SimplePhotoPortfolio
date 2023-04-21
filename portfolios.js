f609037f4d4fac547a1de8a6272eeb74a97ecb46
Sam Jentsch <samclayj@gmail.com>
2023-04-21 12:53:39 -0500

    Refactor portfolio component

--- portfolios.js
+++ portfolios.js
@@ -15,118 +15,7 @@ const fearOfWater = [
   'Meeting 5 - Sam Jentsch 13.jpg',
 ];
 
-let galleryElement;
-let currentGallery = fearOfWater;
-let galleryIndex = 0;
-let lastLoaded = 0;
-// Use this to resize the images as needed.
-let width = window.innerWidth;
-
 const MOBILE_WIDTH = 1000;
-
-function isMobile() {
-  console.log(`isMobile? ${width <= MOBILE_WIDTH}`);
-  return width <= MOBILE_WIDTH;
-}
-
-document.onkeydown = arrowKeyNav;
-
-/**
- * Netlify CDN/LMS doesn't seemt to cache images with this low request level.
- * https://answers.netlify.com/t/netlify-large-media-ttfb/48948/13
- * Try to preload the images to optimize.
- */
-function preloadImages() {
-  preload1 = document.getElementById('pre1');
-  preload2 = document.getElementById('pre2');
-  preload3 = document.getElementById('pre3');
-  preload4 = document.getElementById('pre4');
-  preload1.style.backgroundImage = getCssUrl(currentGallery[incrementPreloadIndex()]);
-  preload2.style.backgroundImage = getCssUrl(currentGallery[incrementPreloadIndex()]);
-  preload3.style.backgroundImage = getCssUrl(currentGallery[incrementPreloadIndex()]);
-  preload4.style.backgroundImage = getCssUrl(currentGallery[incrementPreloadIndex()]);
-
-}
-
-function getUrl(imagePath) {
-  const galleryName = 'fear_of_water';
-  return `static/${galleryName}/${imagePath}?nf_resize=fit&w=${Math.round(width * .9)}`;
-  return "url('static/fear_of_water/" + imagePath + "?nf_resize=fit&w=" + Math.round(width * .9) + "')";
-}
-
-function getCssUrl(imagePath) {
-  return `url('${getUrl(imagePath)}')`;
-}
-
-function setImage(imagePath) {
-  galleryElement.classList.remove('fade');
-  galleryElement.style.backgroundImage = getCssUrl(imagePath);
-  setTimeout(() => {
-    galleryElement.classList.add('fade');
-  }, 100);
-}
-
-function incrementPreloadIndex() {
-  lastLoaded = (lastLoaded + 1) % currentGallery.length;
-  return lastLoaded;
-}
-
-function getNextIndex() {
-  return (galleryIndex + 1) % currentGallery.length;
-}
-
-function nextImage() {
-  galleryIndex = getNextIndex();
-  setImage(currentGallery[galleryIndex]);
-  if (galleryIndex == lastLoaded) {
-    preloadImages();
-  }
-}
-
-function prevImage() {
-  galleryIndex = galleryIndex != 0 ? galleryIndex - 1 : currentGallery.length - 1;
-  setImage(currentGallery[galleryIndex]);
-}
-
-function arrowKeyNav(e) {
-  if (e.keyCode == '37') {
-    // left arrow
-    prevImage();
-  }
-  else if (e.keyCode == '39') {
-    // right arrow
-    nextImage();
-  }
-}
-
-let mobileElements = [];
-function configureMobileGallery(delay) {
-  console.log('Configure gallery...');
-  const container = document.querySelector('.mobile-container');
-  container.innerHTML = '';
-  for (const imagePath of currentGallery) {
-    console.log(`Adding image ${imagePath}`);
-    const image = new Image();
-    image.loading = 'lazy';
-    image.src = getUrl(imagePath);
-    image.classList.add('mobile-image');
-    container.appendChild(image);
-    mobileElements.push(image);
-  }
-  setTimeout(() => {
-    fadeIn();
-  }, delay);
-}
-
-function init(delay) {
-  if (isMobile()) {
-    // Configure gallery with images appended to DOM.
-    configureMobileGallery(delay);
-  } else {
-    // Configure gallery with paginated images.
-    preloadImages();
-  }
-}
 
 const debounce = (callback, wait) => {
   let timeoutId = null;
@@ -138,28 +27,164 @@ const debounce = (callback, wait) => {
   };
 }
 
-window.onresize = debounce((ev) => {
-  if (window.innerWidth != width) {
-    width = window.innerWidth;
-    init(500);
-  }
-}, 250);
-
-window.onload = () => {
-  galleryIndex = 0;
-  galleryElement = document.getElementById('gallery');
-  setImage(currentGallery[0]);
-  init(1000);
+function getUrl(imagePath, width) {
+  const galleryName = 'fear_of_water';
+  return `static/${galleryName}/${imagePath}?nf_resize=fit&w=${Math.round(width * .9)}`;
+  return "url('static/fear_of_water/" + imagePath + "?nf_resize=fit&w=" + Math.round(width * .9) + "')";
 }
 
-window.addEventListener('scroll', fadeIn);
-function fadeIn() {
-  for (const el of mobileElements) {
-    const distInView = el.getBoundingClientRect().top - window.innerHeight + 20;
-    if (distInView < 0) {
-      el.classList.add("fade");
-    } else {
-      el.classList.remove("fade");
+function getCssUrl(imagePath) {
+  return `url('${getUrl(imagePath)}')`;
+}
+
+
+fetch("portfolio.html")
+  .then(stream => stream.text())
+  .then(text => definePortfolio(text));
+
+function definePortfolio(html) {
+  class CustomPortfolio extends HTMLElement {
+    constructor() {
+      super();
+      const shadow = this.attachShadow({ mode: 'open' });
+      shadow.innerHTML = html;
+
+      this.mobileElements = [];
+      this.galleryElement = this.shadowRoot.getElementById('gallery');
+      this.currentGallery = fearOfWater;
+      this.galleryIndex = 0;
+      this.lastLoaded = 0;
+
+      // Use this to resize the images as needed.
+      this.width = window.innerWidth;
+
+      this.shadowRoot.querySelector('.footer-left')
+        .addEventListener('click', e => {this.nextImage()});
+      this.shadowRoot.querySelector('.footer-right')
+        .addEventListener('click', e => {this.prevImage()});
+      document.addEventListener('keydown', e => this.arrowKeyNav(e))
+      window.addEventListener('scroll', debounce(e => {
+        console.log('scroll');
+        this.fadeIn();
+      }, 100));
+      window.addEventListener('resize', debounce(e => {
+        console.log('resize');
+        if (window.innerWidth != this.width) {
+          this.width = window.innerWidth;
+          this.init(1000);
+        }
+      }, 250));
+
+      // Also need to fix the arrow key nav.
+    }
+
+    connectedCallback() {
+      this.galleryIndex = 0;
+      this.galleryElement = this.shadowRoot.getElementById('gallery');
+      this.setImage(this.currentGallery[0]);
+      this.init(1500);
+    }
+
+    isMobile() {
+      return this.width <= MOBILE_WIDTH;
+    }
+
+    /**
+     * Netlify CDN/LMS doesn't seemt to cache images with this low request level.
+     * https://answers.netlify.com/t/netlify-large-media-ttfb/48948/13
+     * Try to preload the images to optimize.
+     */
+    preloadImages() {
+      let preload1 = this.shadowRoot.getElementById('pre1');
+      let preload2 = this.shadowRoot.getElementById('pre2');
+      let preload3 = this.shadowRoot.getElementById('pre3');
+      let preload4 = this.shadowRoot.getElementById('pre4');
+      preload1.style.backgroundImage = getCssUrl(this.currentGallery[this.incrementPreloadIndex()]);
+      preload2.style.backgroundImage = getCssUrl(this.currentGallery[this.incrementPreloadIndex()]);
+      preload3.style.backgroundImage = getCssUrl(this.currentGallery[this.incrementPreloadIndex()]);
+      preload4.style.backgroundImage = getCssUrl(this.currentGallery[this.incrementPreloadIndex()]);
+
+    }
+
+    setImage(imagePath) {
+      this.galleryElement.classList.remove('fade');
+      this.galleryElement.style.backgroundImage = getCssUrl(imagePath);
+      setTimeout(() => {
+        this.galleryElement.classList.add('fade');
+      }, 100);
+    }
+
+    incrementPreloadIndex() {
+      this.lastLoaded = (this.lastLoaded + 1) % this.currentGallery.length;
+      return this.lastLoaded;
+    }
+
+    getNextIndex() {
+      return (this.galleryIndex + 1) % this.currentGallery.length;
+    }
+
+    nextImage() {
+      this.galleryIndex = this.getNextIndex();
+      this.setImage(this.currentGallery[this.galleryIndex]);
+      if (this.galleryIndex == this.lastLoaded) {
+        this.preloadImages();
+      }
+    }
+
+    prevImage() {
+      this.galleryIndex = this.galleryIndex != 0 ? this.galleryIndex - 1 : this.currentGallery.length - 1;
+      this.setImage(this.currentGallery[this.galleryIndex]);
+    }
+
+    arrowKeyNav(e) {
+      if (e.keyCode == '37') {
+        // left arrow
+        this.prevImage();
+      }
+      else if (e.keyCode == '39') {
+        // right arrow
+        this.nextImage();
+      }
+    }
+
+    configureMobileGallery(delay) {
+      const container = this.shadowRoot.querySelector('.mobile-container');
+      container.innerHTML = '';
+      for (const imagePath of this.currentGallery) {
+        console.log(`Adding image ${imagePath}`);
+        const image = new Image();
+        image.loading = 'lazy';
+        image.src = getUrl(imagePath, this.width);
+        image.classList.add('mobile-image');
+        container.appendChild(image);
+        this.mobileElements.push(image);
+      }
+      setTimeout(() => {
+        this.fadeIn();
+      }, delay);
+    }
+
+    init(delay) {
+      if (this.isMobile()) {
+        // Configure gallery with images appended to DOM.
+        this.configureMobileGallery(delay);
+      } else {
+        // Configure gallery with paginated images.
+        this.preloadImages();
+      }
+    }
+
+    fadeIn() {
+      for (const el of this.mobileElements) {
+        const distInView = el.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+          el.classList.add("fade");
+        } else {
+          el.classList.remove("fade");
+        }
+      }
     }
   }
+
+  window.customElements.define('custom-portfolio', CustomPortfolio);
 }
